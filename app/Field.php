f81f28a1bf3235782a51aff7ae585a043f523457
Brad Ahrens <bradleyjamesahrens@gmail.com>
2020-01-25 23:41:41 +0000

    chore: models, migrations, and factories

--- app/Field.php
+++ app/Field.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Field extends Model
 {
+    /**
+     * Get the values for the field.
+     */
+    public function values()
+    {
+        return $this->hasMany('App\FieldValue');
+    }
+
     public $timestamps = false;
     protected $fillable = [
         'title',
