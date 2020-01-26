a5bd03ea2e3a4f917bdb0b438495881d4147121a
Brad Ahrens <bradleyjamesahrens@gmail.com>
2020-01-26 14:26:30 +0000

    chore: setup hot reloading

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,26 +19,43 @@
                     <div class="col-12">
                         <ul class="nav nav-tabs">
                             <li class="nav-item">
-                                <a class="nav-link active" href="#">Active</a>
+                                <a class="nav-link active" href="#">Subscribers</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#">Disabled</a>
+                                <a class="nav-link" href="#">Fields</a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
-            <ul>
-                @foreach ($subscribers as $subscriber)
-                    <li>{{ $subscriber->email }}</li>
-                @endforeach
-            </ul>
+            <div class="container" id="subscribers">
+                <div class="row">
+                    <div class="col-12">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th class="border-top-0" scope="col">#</th>
+                                    <th class="border-top-0" scope="col">Email</th>
+                                    <th class="border-top-0" scope="col">First Name</th>
+                                    <th class="border-top-0" scope="col">Last Name</th>
+                                    <th class="border-top-0" scope="col">State</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($subscribers as $subscriber)
+                                <tr>
+                                    <th scope="row">{{ $subscriber->id }}</th>
+                                    <td>{{ $subscriber->email }}</td>
+                                    <td>{{ $subscriber->first_name }}</td>
+                                    <td>{{ $subscriber->last_name }}</td>
+                                    <td>{{ $subscriber->state }}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
 
         </div>
         <script src="{{ mix('js/app.js')}}"></script>
